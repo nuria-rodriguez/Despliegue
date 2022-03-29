f70e6d00cb5c95984dea10237a490e931dc43e31
nuria-rodriguez <98884358+nuria-rodriguez@users.noreply.github.com>
2022-03-29 19:33:15 +0200

    Add files via upload

---
+++ Productos/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"EjerProductos","l":"NoPerecedero"},{"p":"EjerProductos","l":"Perecedero"},{"p":"EjerProductos","l":"Productos"},{"p":"EjerProductos","l":"TestProducto"}];updateSearchResults();
